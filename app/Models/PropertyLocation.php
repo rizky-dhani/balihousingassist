cab4625a405dbf5fead759e4aed49f175c9ef0b6
Rizky Dhani <rizkydhni03@gmail.com>
2026-02-13 17:33:38 +0700

    Auto-generate slug for PropertyLocation model

--- app/Models/PropertyLocation.php
+++ app/Models/PropertyLocation.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use App\Traits\InteractsWithSeo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class PropertyLocation extends Model
 {
@@ -20,6 +21,23 @@ class PropertyLocation extends Model
         'longitude',
         'parent_id',
     ];
+
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        static::creating(function (PropertyLocation $location): void {
+            if (empty($location->slug)) {
+                $location->slug = Str::slug($location->name);
+            }
+        });
+
+        static::updating(function (PropertyLocation $location): void {
+            if ($location->isDirty('name') && empty($location->slug)) {
+                $location->slug = Str::slug($location->name);
+            }
+        });
+    }
 
     public function getRouteKeyName(): string
     {
